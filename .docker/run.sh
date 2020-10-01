7702676d320d0b09782db6090d254a370cbf55ad
Keshari Nandan <keshari.nandan2@gmail.com>
2020-10-01 21:07:36 +0530

    Added run.sh

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+ARG AWS_SECRET_ACCESS_KEY
+ARG AWS_ACCESS_KEY_ID
+ARG AWS_DEFAULT_REGION
+ARG PORTFOLIO_REPOSITORY
+
+echo "Setting alias for Zappa"
+# shellcheck disable=SC2139
+alias zappashell3="docker run -ti -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} -v \"$(pwd):/var/task\"  --rm lambci/lambda:build-python3.8 bash"
+alias zappashell3 >> ~/.bash_profile
+
+# shellcheck disable=SC2139
+alias zappashell="docker run -ti -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} -v \"$(pwd):/var/task\" -v ~/.aws/:/root/.aws  --rm ${PORTFOLIO_REPOSITORY}"
+alias zappashell >> ~/.bash_profile
+
 echo "Creating virtual environment..."
-python -m venv ve
+python -m venv venv
 
 echo "Activating virtual environment..."
-source ve/bin/activate
+source venv/bin/activate
 
 echo "Installing the requirements..."
 pip install -r requirements.txt
 
-echo "Checking the deployment status"
-zappa status dev
+echo "Success...."
